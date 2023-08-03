5d27abc164ee5c8148592f7b89f34070eb3b3309
suryacodes <62347567+suryacodes@users.noreply.github.com>
2023-08-03 10:32:25 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-import React from 'react';
-import './style.css';
+import { useEffect, useState } from 'react';
+import './App.css';
 
-export default function App() {
+const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function App() {
+  const [board, setBoard] = useState(() =>
+    Array.from({ length: 9 }, () => null)
+  );
+  const [player, setPlayer] = useState(1);
+  const [winner, setWinner] = useState(null);
+
+  const onChange = (index) => {
+    if (board[index] || winner) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = player === 1 ? 'X' : 'O';
+    setBoard(newBoard);
+    setPlayer((prevPlayer) => (prevPlayer === 1 ? 2 : 1));
+  };
+
+  useEffect(() => {
+    for (const [a, b, c] of lines) {
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        setWinner(player);
+        return;
+      }
+    }
+
+    if (board.every((cell) => cell !== null)) {
+      setWinner('Draw');
+    }
+  }, [board, player]);
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <div className="board">
+        {board?.map((cell, index) => (
+          <button onClick={() => onChange(index)} key={index}>
+            {cell}
+          </button>
+        ))}
+      </div>
+      {winner && (
+        <div>
+          {winner === 'Draw' ? "It's a draw!" : `The winner is ${winner}`}
+        </div>
+      )}
     </div>
   );
 }
+
+export default App;
